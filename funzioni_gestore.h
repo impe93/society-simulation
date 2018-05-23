ebcbcb260a01e80bed2d3684f1ca95787bbac2a8
Lorenzo Imperatrice <lorenzo@MacBook-Pro-di-Lorenzo.local>
2018-05-23 15:47:03 +0200

    Modifiche a makefile funzioni_gestore
    * makefile: aggiunte tutte le dipendenze corrette
    * funzione_gestore: aggiunta la definizione  e la descrizione di alcuni
    metodi che non erano stati precedente aggiunti

--- funzioni_gestore.h
+++ funzioni_gestore.h
@@ -28,6 +28,9 @@ bool isUnsignedNumber(char* stringa);
  * 
  * @param {caratteristiche_individuo} individuo: la struttura che ha specificate le caratteristiche
  * dell'individuo
+ * @param {int} init_peolple: il numero massimo d'individui all'interno della simulazione
+ * @param {int} inizializzazione: la modalità di inizializzazione che deve avere l'individuo
+ * che si sta avviando
  */
 void avvia_individuo (caratteristiche_individuo individuo, int init_people, int inizializzazione);
 
@@ -36,23 +39,23 @@ void avvia_individuo (caratteristiche_individuo individuo, int init_people, int 
  * 
  * @param {unsigned long} genes: Rappresenta fino a che numero un gene può arrivare, serve per
  * limitare il valore casuale
- * @param {int} init_people: 
+ * @param {int} init_people: il numero massimo d'individui all'interno della simulazione
+ * @param {char} tipo: Il tipo dell'individuo che si sta creando che può essere A, B
+ * oppure C in caso non sia da impostare
  */
 caratteristiche_individuo crea_individuo (unsigned long genes, int init_people, char tipo);
 
 /**
- * Inizializza tanti individui in base a quanto è segnato nel parametro init_people
+ * Inizializza init_people individui prima di cominciare la simulazione
  * 
  * @param {int} init_people: Il numero di individui da inizializzare
  * @param {unsigned long} genes: Il massimo valore che può assumere il genoma di un individuo.
- * @param {pid_t []} individui_da_avviare: Ad ogni creazione di un individuo il suo pid viene messo
- * all'interno di questo array.
  */
 void inizializza_individui(int init_people, unsigned long genes);
 
 /**
  * Inizializza l'array di rappresentazioni degli individui di un detrminato tipo
- * con valori di default
+ * con dei valori per evitare quelli di default
  * 
  * @param {int} shm_id: L'ID della shared memory di individui da inizializzare
  * @param {int} init_people: Il numero di individui totali che saranno presenti
@@ -67,7 +70,7 @@ void inizializza_shm(int shm_id, int init_people);
  * @param {int} init_people: la massima quantità di elementi che può essere presente
  * all'interno dell'array
  * @param {int} pid_gestore: Il pid del processo gestore
- * @param {int} pid_terminatore: Il pid del processo terminatore
+ * @param {int} pid_terminatore_processi: Il pid del processo terminatore
  * 
  * @error: Conclude il processo.
  */
@@ -117,6 +120,8 @@ int numero_random(unsigned int min, unsigned int max);
  * trova quello da terminare
  * @param {int}: il numero dell'individuo, tra gli individui utilizzati, scelto per
  * essere terminato
+ * @param {descrizione_simulazione} descrizione: La struttura che contiene le informazioni
+ * inerenti la simulazione
  */
 void termina_individuo(rappresentazione_individuo individui [], int numero_da_terminare, descrizione_simulazione* descrizione);
 
@@ -128,17 +133,17 @@ void termina_individuo(rappresentazione_individuo individui [], int numero_da_te
  * su cui cercare il processo da terminare
  * @param {int} birth_death: L'intervallo di tempo in secondi che deve passare tra una
  * terminazione e l'altra
- * @param {descrizione_individui} descrizione: Una struttura che deve essere aggiornata
+ * @param {descrizione_individui*} descrizione: Una struttura che deve essere aggiornata
  * ad ogni terminazione e ad ogni creazione di un nuovo individuo.
  */
 void attivita_terminatore_individui(int init_people, int birth_death, unsigned long genes, descrizione_simulazione* descrizione_simulazione);
 
 /**
- * Chiamato quando un processo casuale di tipo A deve terminare, il metodo lo sceglie,
- * lo termina ed aggiorna la descrizione.
+ * Chiamato quando un processo casuale deve terminare, il metodo lo sceglie, lo termina ed aggiorna la descrizione.
  * 
  * @param {int} init_people: La lunghezza massima che può avere l'array di individui
  * @param {descrizione_simulazione*} descrizione: La struttura che descrive la simulazione
+ * @param {char} tipo_terminazione: Il tipo che andrà a terminare
  */
 void preparazione_terminazione(int init_people, descrizione_simulazione* descrizione, char tipo_terminazione);
 
@@ -174,21 +179,67 @@ void aggiorna_descrizione_nuovo_individuo(caratteristiche_individuo individuo);
 void aggiorna_utente_stato_simulazione();
 
 /**
- * Recupera l'individuo con pid uguale al parametro "pid_a" dalla shm A
+ * Recupera l'individuo con pid uguale al parametro "pid" dalla shm
  * 
- * @param {pid_t} pid_a: Il pid di un individuo di tipo A che si trova sulla shm A
+ * @param {pid_t} pid: Il pid di un individuo di tipo A che si trova sulla shm A
  * @param {int} init_people: Il numero totale di individui
+ * @param {descrizione_simulazione*} descrizione: La struttura che descrive la simulazione
+ * @param {int} sem_shm_descrizione_id: L'ID del semaforo che regola l'accesso in mutua esclusione
+ * @param {char} tipo: Il tipo da recuperare
+ * 
  * @return {caratteristiche_individuo}: Le caratteristiche dell'individuo trovato
  * nella shm con PID uguale al valore passato come parametro. Individuo vuoto se
  * non è stato trovato l'individuo con il PID desiderato
  */
-caratteristiche_individuo recupera(pid_t pid_a, int init_people, descrizione_simulazione* descrizione, int sem_shm_descrizione_id, char tipo);
+caratteristiche_individuo recupera(pid_t pid, int init_people, descrizione_simulazione* descrizione, int sem_shm_descrizione_id, char tipo);
 
+/**
+ * Calcola il massimo comun divisore tra a e b e lo restituisce
+ * 
+ * @param {unsigned long} a: Il primo numero da confrontare
+ * @param {unsigned long} b: Il secondo numero da confrontare
+ * 
+ * @return il massimo comun divisore
+ */
+unsigned long mcd(unsigned long a, unsigned long b);
 
+/**
+ * Calcola il genoma che avrà il figlio della coppia
+ * 
+ * @param {unsigned long} genoma_individuo_a: il genoma dell'individuo A che si è accoppiato
+ * @param {unsigned long} genoma_individuo_b: il genoma dell'individuo B che si è accoppiato
+ * @param {unsigned long} genes: il valore di genes iniziale
+ * 
+ * @return Il genoma che avrà il figlio tra A e B
+ */
+unsigned long genoma_nuovo_da_coppia(unsigned long genoma_individuo_a, unsigned long genoma_individuo_b, unsigned long genes);
 
+/**
+ * Crea il nome che avrà il figlio di una coppia dopo l'accoppiamento e lo assegna al
+ * parametro nome_nuovo_individuo
+ * 
+ * @param {bool} scelta_nome: Il genitore da cui viene derivato il nome del figlio
+ * @param {char*} nome_nuovo_individuo: Il nome del nuovo individuo
+ * @param {char*} nome_a: Il nome del genitore A
+ * @param {char*} nome_b: Il nome del genitore B
+ */
+void nome_nuovo_da_coppia(bool scelta_nome, char* nome_nuovo_individuo, char* nome_a, char* nome_b);
 
-
-
+/**
+ * Crea le caratteristiche di un nuovo individuo che si sta per creare da una coppia
+ * 
+ * @param {caratteristiche_individuo*} nuovo_individuo: La struttura del nuovo individuo che si sta creando
+ * @param {caratteristiche_individuo*} individuo_a: Le caratteristiche del genitore A
+ * @param {caratteristiche_individuo*} individuo_b: Le caratteristiche del genitore B
+ * @param {unsigned long} genes: il valore di genes iniziale
+ * @param {int} scelta_nome: Viene utilizzato per stabilire il genitore da cui viene ereditato parte del nome
+ * @param {rappresentazione_individuo*} individui_B: L'array contenente gli individui B
+ * @param {rappresentazione_individuo*} individui_A: L'array contenente gli individui A
+ * @param {int} init_people: Il numero totale di individui
+ * @param {char*} tipo_scelto: il tipo che è stato precedentemente scelto C se non è stato ancora scelto
+ * nessun tipo precedentemente
+ */
+void crea_individuo_da_coppia(caratteristiche_individuo* nuovo_individuo, caratteristiche_individuo individuo_a,caratteristiche_individuo individuo_b, unsigned long genes, bool scelta_nome, rappresentazione_individuo* individui_A, rappresentazione_individuo* individui_B, int init_people, char* tipo_scelto);
 
 // Definizione metodi
 
--- makefile
+++ makefile
@@ -1,12 +1,12 @@
 socety-simulation: tipo_A tipo_B gestore
 
-tipo_A: tipo_A.c tipi_simulatore_societa.h gestione_code.h gestione_semafori.h gestione_shm.h funzioni_A_B.h
+tipo_A: tipo_A.c tipi_simulatore_societa.h gestione_code.h gestione_semafori.h gestione_shm.h funzioni_A_B.h sem_protocol.h
 	gcc tipo_A.c -o tipo_A
 
-tipo_B: tipo_B.c tipi_simulatore_societa.h gestione_code.h gestione_semafori.h gestione_shm.h funzioni_A_B.h funzioni_B.h
+tipo_B: tipo_B.c tipi_simulatore_societa.h gestione_code.h gestione_semafori.h gestione_shm.h funzioni_A_B.h funzioni_B.h sem_protocol.h
 	gcc tipo_B.c -o tipo_B
 
-gestore: gestore.o
+gestore: gestore.c sem_protocol.h gestione_shm.h gestione_semafori.h tipi_simulatore_societa.h gestione_code.h funzioni_gestore.h
 	gcc gestore.c -o gestore
 
 clean:
